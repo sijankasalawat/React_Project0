4138b57abc460cf6bb4f832ed19923c36676e89d
sijan <sijankasalawat3339@gmail.com>
2023-05-22 22:34:48 +0545

    making different component in dashboard

--- my-app/src/Fiber_Star/Dashboard/loginpage.js
+++ my-app/src/Fiber_Star/Dashboard/loginpage.js
@@ -1,4 +1,7 @@
 import "./login.css";
+import Rating from './Rating';
+import { ReactDOM } from "react";
+
 function loginpage() {
   const rate = 4.5;
   return (
@@ -26,28 +29,8 @@ function loginpage() {
         </header>
         <div className="mainContainer container d-flex">
           <div className="left_container">
-            <div className="rate">
-              <input type="radio" id="star5" name="rate" value="5" />
-              <label for="star5" title="text">
-                5 stars
-              </label>
-              <input type="radio" id="star4" name="rate" value="4" />
-              <label for="star4" title="text">
-                4 stars
-              </label>
-              <input type="radio" id="star3" name="rate" value="3" />
-              <label for="star3" title="text">
-                3 stars
-              </label>
-              <input type="radio" id="star2" name="rate" value="2" />
-              <label for="star2" title="text">
-                2 stars
-              </label>
-              <input type="radio" id="star1" name="rate" value="1" />
-              <label for="star1" title="text">
-                1 star
-              </label>
-            </div>
+            <Rating/>
+          
 
             <p> Rated {rate}/5 (243 reviews) </p>
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Fiber_Star/loginpage';
+import Login from './Fiber_Star/Dashboard/loginpage';
+import Rating from './Fiber_Star/Dashboard/Rating';
+
+
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,6 +25,10 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path:"/rating",
+    element:<Rating/>
   }
 ]);
 
@@ -32,6 +39,7 @@ root.render(
    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
