a163631d5a8051b8cddc281dd396a0c694ec9536
sijan <sijankasalawat3339@gmail.com>
2023-05-31 15:23:03 +0545

    adding list updated

--- my-app/src/todolist/todolists.js
+++ my-app/src/todolist/todolists.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
-
+import './todolist.css';
 
 const ToDoList = () => {
-    const [input, setInput] = React.useState('');
-  
-    const handleInputChange = (e) => {
-      setInput(e.target.value);
-    }
-  
-    const Inputs =()=>{
-      return(
-        <div>
-          <input type="text" value={input} onChange={handleInputChange} />
-          <button>Add</button>
-        </div>
-      )
-    }
-  
+  const [input, setInput] = useState('');
+  const [counter, setCounter] = useState('');
+  const [list, setList] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   }
-  
+
+  const handleAdd = () => {
+    const newItem = { input, counter };
+    setList([...list, newItem]);
+    setCounter(counter + input);
+    setInput('');
+  }
+
+  return (
+    <div className='main-w'>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleAdd}>Add</button>
+      
+      {list.map((item, index) => (
+        <div key={index} className="row">{item.input}
+        
+          </div>
+          
+      ))}
+
+    </div>
+  );
+}
+
 export default ToDoList;
