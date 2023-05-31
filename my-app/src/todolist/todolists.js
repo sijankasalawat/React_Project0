3fdeb5e309092fc004f1fbd43faef24d5314c255
sijan <sijankasalawat3339@gmail.com>
2023-05-31 21:02:32 +0545

    todolist is finish

--- my-app/src/todolist/todolists.js
+++ my-app/src/todolist/todolists.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-import './todolist.css';
+import React, { useState } from "react";
+import "./todolist.css";
 
 const ToDoList = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [list, setList] = useState([]);
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
-  }
-  
+  };
 
   const handleAdd = () => {
     const newItem = { input };
     setList([...list, newItem]);
-    setInput('');
-    
-   
-  }
+    setInput("");
+  };
 
   const handleDelete = (index) => {
     const updatedList = [...list];
     updatedList.splice(index, 1);
     setList(updatedList);
-  }
+  };
 
   const handleEdit = (index) => {
     const editedItem = list[index];
@@ -32,51 +29,67 @@ const ToDoList = () => {
       setInput(editedItem.input);
       return newList;
     });
-  }
+  };
+  const num = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
 
   return (
     <>
-     <div className='main-w container'>
-      <h1 >To Do List</h1>
+    <div className="mainscreen">
+      <div className="main-w container">
+        <h1 className="heading text-center ">To Do List</h1>
+        <div className="days bg-primary text-white"></div>
 
-      <div className="lists w-100 d-flex align-items-center">
-      <input className=' inputs ' type="text" value={input} onChange={handleInputChange} />
-      <button onClick={handleAdd}>Add</button>
-
-      </div>
-     
-      
-      {list.map((item, index) => (
-      
-        <div key={index} className=" Lists d-flex justify-content-between">
-          {item.editing ? (
-            <>
-              
-              <input
-                type="text"
-                value={input}
-                onChange={handleInputChange}
-              />
-              <button onClick={handleAdd}>Save</button>
-            </>
-          ) : (
-            <>
-              <div>{item.input}</div>
-              <div>
-              <button onClick={() => handleEdit(index)}>Edit</button>
-              <button onClick={() => handleDelete(index)}>Delete</button>
-
-              </div>
-            
-
-            </>
-          )}
+        <div className="lists  d-flex align-items-center ">
+          <input className="inp  " placeholder="Add a task" type="text" value={input} onChange={handleInputChange} />
+          <button className ="adds  " onClick={handleAdd}>Add</button>
         </div>
-      ))}
-    </div>
+        <div className="con " id="containers">
+          {list.map((item, index) => (
+            <div
+              key={index}
+              className=" lists  d-flex justify-content-between text-white" >
+              {item.editing ? (
+                <>
+                  <input type="text"value={input} onChange={handleInputChange}/>
+                  <button onClick={handleAdd}>Save</button>
+                </>
+              ) : (
+                <>
+                <div className="inputss">
+                <a className="days text-white" link="/">
+                    {num[index]}.
+                  </a>
+                  <div className="itemInput text-white">{item.input}</div>
+                </div>
+                
+                 
+                  <div className="btns">
+                    <button
+                      className="edit btn "
+                      onClick={() => handleEdit(index)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="delete btn"
+                      onClick={() => handleDelete(index)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                 
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      </div>
     </>
-   
   );
-}
+};
 
 export default ToDoList;
