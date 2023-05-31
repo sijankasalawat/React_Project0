5893a18f066b1aac2d184430f0fc9b17b915b295
sijan <sijankasalawat3339@gmail.com>
2023-05-31 16:30:43 +0545

    edditing button is updated

--- my-app/src/todolist/todolists.js
+++ my-app/src/todolist/todolists.js
@@ -3,33 +3,79 @@ import './todolist.css';
 
 const ToDoList = () => {
   const [input, setInput] = useState('');
-  const [counter, setCounter] = useState('');
   const [list, setList] = useState([]);
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
   }
+  
 
   const handleAdd = () => {
-    const newItem = { input, counter };
+    const newItem = { input };
     setList([...list, newItem]);
-    setCounter(counter + input);
     setInput('');
+    
+   
+  }
+
+  const handleDelete = (index) => {
+    const updatedList = [...list];
+    updatedList.splice(index, 1);
+    setList(updatedList);
+  }
+
+  const handleEdit = (index) => {
+    const editedItem = list[index];
+    setList((prevList) => {
+      const newList = [...prevList];
+      newList.splice(index, 1);
+      setInput(editedItem.input);
+      return newList;
+    });
   }
 
   return (
-    <div className='main-w'>
-      <input type="text" value={input} onChange={handleInputChange} />
+    <>
+     <div className='main-w container'>
+      <h1 >To Do List</h1>
+
+      <div className="lists w-100 d-flex align-items-center">
+      <input className=' inputs ' type="text" value={input} onChange={handleInputChange} />
       <button onClick={handleAdd}>Add</button>
+
+      </div>
+     
       
       {list.map((item, index) => (
-        <div key={index} className="row">{item.input}
-        
-          </div>
-          
-      ))}
+      
+        <div key={index} className=" Lists d-flex justify-content-between">
+          {item.editing ? (
+            <>
+              
+              <input
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+              />
+              <button onClick={handleAdd}>Save</button>
+            </>
+          ) : (
+            <>
+              <div>{item.input}</div>
+              <div>
+              <button onClick={() => handleEdit(index)}>Edit</button>
+              <button onClick={() => handleDelete(index)}>Delete</button>
 
+              </div>
+            
+
+            </>
+          )}
+        </div>
+      ))}
     </div>
+    </>
+   
   );
 }
 
